87628d6bc82d755f8974386062b72d03892989b9
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2025-02-15 13:01:12 +0530

    Update dbconnection.js

--- OneDrive/Desktop/full_stack/socketio/backend/dbconnection.js
+++ OneDrive/Desktop/full_stack/socketio/backend/dbconnection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-const dbconnectionjs=mongoose.connect('mongodb+srv://Hafez7865:Hafez7865@cluster0.oh20y.mongodb.net/socketio')
+const dbconnectionjs=mongoose.connect('mongodb+srv://***:***@cluster0.oh20y.mongodb.net/socketio')
 .then(()=>console.log("connection created with db done")
 )
 .catch(()=>console.log("Problem when connect with connection created"))
